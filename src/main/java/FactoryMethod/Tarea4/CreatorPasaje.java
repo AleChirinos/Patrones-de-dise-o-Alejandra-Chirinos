9694bde10a499ec4004cd1857c06aceac8549464
alejandra-chirinos <alejandra.chirinos2702@gmail.com>
2021-05-20 22:16:29 -0400

    Tarea 4 (Ejercicio Factory Method) - Alejandra Chirinos

---
+++ src/main/java/FactoryMethod/Tarea4/CreatorPasaje.java
@@ -0,0 +1,6 @@
+package FactoryMethod.Tarea4;
+
+public abstract class CreatorPasaje {
+    public CreatorPasaje(){}
+    public abstract IPasaje create();
+}
---
+++ src/main/java/FactoryMethod/Tarea4/IPasaje.java
@@ -0,0 +1,5 @@
+package FactoryMethod.Tarea4;
+
+public interface IPasaje {
+    void crear();
+}
