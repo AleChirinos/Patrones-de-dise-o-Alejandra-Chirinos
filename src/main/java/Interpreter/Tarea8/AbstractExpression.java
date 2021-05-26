b5f5f750facf75fef10737c68e5a906ad850f726
alejandra-chirinos <alejandra.chirinos2702@gmail.com>
2021-05-25 23:52:15 -0400

    Tarea 8 (Ejercicio Interpreter) - Alejandra Chirinos

---
+++ src/main/java/Interpreter/Tarea8/AbstractExpression.java
@@ -0,0 +1,5 @@
+package Interpreter.Tarea8;
+
+public abstract class AbstractExpression {
+    public abstract void interpreter(ContextNumerosRomanos context);
+}
---
+++ src/main/java/Interpreter/Tarea8/ContextNumerosRomanos.java
@@ -0,0 +1,9 @@
+package Interpreter.Tarea8;
+
+public class ContextNumerosRomanos {
+    protected String input = "";
+    protected String output = "";
+    public ContextNumerosRomanos(String input){
+        this.input=input;
+    }
+}
